0ed558cdbe0e1c94d449f6760aa5ccbe6d553d9d
Volodymyr Bolshakov <volodbol@gmail.com>
2022-12-23 21:05:02 +0200

    feat: add project skeleton

---
+++ src/main/java/com/task/broker/SimpleBrokerTaskApplication.java
@@ -0,0 +1,13 @@
+package com.task.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBrokerTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBrokerTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/broker/SimpleBrokerTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBrokerTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
