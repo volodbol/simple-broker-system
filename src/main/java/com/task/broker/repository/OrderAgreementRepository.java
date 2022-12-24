dca55deb9014074293f1f21ece89094c2bba32ba
Volodymyr Bolshakov <volodbol@gmail.com>
2022-12-25 00:44:13 +0200

    fix: change order of parameters

--- src/main/java/com/task/broker/repository/OrderAgreementRepository.java
+++ src/main/java/com/task/broker/repository/OrderAgreementRepository.java
@@ -15,7 +15,7 @@ public interface OrderAgreementRepository extends JpaRepository<OrderAgreement, 
     List<OrderAgreement> findAllByOrder(Order order);
 
 
-    @Query("select o from OrderAgreement o where o.firstOrder = ?1 or o.secondOrder = ?1 and o.isPerformed = false")
+    @Query("select o from OrderAgreement o where o.isPerformed = false and o.firstOrder = ?1 or o.secondOrder = ?1")
     List<OrderAgreement> findAllByOrderAndIsPerformedFalse(Order order);
 
 }
