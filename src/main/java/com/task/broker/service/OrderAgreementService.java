4f9d40e6bf778d0a140dd1e3995a7a5de3eb8971
Volodymyr Bolshakov <volodbol@gmail.com>
2022-12-24 22:44:20 +0200

    feat: add methods to order agreement service

--- src/main/java/com/task/broker/service/OrderAgreementService.java
+++ src/main/java/com/task/broker/service/OrderAgreementService.java
@@ -1,13 +1,25 @@
 package com.task.broker.service;
 
+import com.task.broker.model.Order;
+import com.task.broker.model.OrderAgreement;
 import com.task.broker.repository.OrderAgreementRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderAgreementService {
 
     private final OrderAgreementRepository orderAgreementRepository;
+
+    private final OrderService orderService;
+
+    public List<OrderAgreement> findAllByMainOrder(Long id) {
+        Order order = orderService.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Can't find order with id - " + id));
+        return orderAgreementRepository.findAllByMainOrder(order);
+    }
 
 }
