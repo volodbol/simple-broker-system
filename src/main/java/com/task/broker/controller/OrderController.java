2a2df2121d0fecf7abc8e7b1939814e8c42b7969
Volodymyr Bolshakov <volodbol@gmail.com>
2022-12-24 22:50:45 +0200

    feat: add method to show agreements

--- src/main/java/com/task/broker/controller/OrderController.java
+++ src/main/java/com/task/broker/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.task.broker.controller;
 import com.task.broker.dto.OrderDto;
 import com.task.broker.model.ApplicationUser;
 import com.task.broker.model.Order;
+import com.task.broker.service.OrderAgreementService;
 import com.task.broker.service.OrderInstrumentService;
 import com.task.broker.service.OrderService;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,6 +27,8 @@ public class OrderController {
 
     private final OrderInstrumentService orderInstrumentService;
 
+    private final OrderAgreementService orderAgreementService;
+
     @GetMapping
     public String allOrders(Model model, @AuthenticationPrincipal ApplicationUser applicationUser) {
         List<Order> orders = orderService.findAllByApplicationUser(applicationUser);
@@ -37,6 +41,12 @@ public class OrderController {
         model.addAttribute("instruments", orderInstrumentService.findAll());
         model.addAttribute("orderDto", OrderDto.builder().build());
         return "create-order";
+    }
+
+    @GetMapping("/{id}/agreements")
+    public String getOrderAgreements(@PathVariable(name = "id") Long orderId, Model model) {
+        model.addAttribute("agreements", orderAgreementService.findAllByMainOrder(orderId));
+        return "order-agreements";
     }
 
     @PostMapping("/save")
