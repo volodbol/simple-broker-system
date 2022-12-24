87284738aa1ec3ac2fa2edd551bdc78629f22884
Volodymyr Bolshakov <volodbol@gmail.com>
2022-12-24 22:53:32 +0200

    feat: add admin method which show agreements

--- src/main/java/com/task/broker/controller/AdminOrderController.java
+++ src/main/java/com/task/broker/controller/AdminOrderController.java
@@ -1,6 +1,7 @@
 package com.task.broker.controller;
 
 import com.task.broker.model.Order;
+import com.task.broker.service.OrderAgreementService;
 import com.task.broker.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -17,10 +18,18 @@ public class AdminOrderController {
 
     private final OrderService orderService;
 
+    private final OrderAgreementService orderAgreementService;
+
     @GetMapping("/orders")
     public String getAllOrders(Model model) {
         model.addAttribute("orders", orderService.findAll());
         return "admin-all-orders";
+    }
+
+    @GetMapping("/orders/{id}/agreements")
+    public String getOrderAgreements(@PathVariable(name = "id") Long orderId, Model model) {
+        model.addAttribute("agreements", orderAgreementService.findAllByMainOrder(orderId));
+        return "admin-order-agreements";
     }
 
     @PostMapping("/orders/{id}/session")
