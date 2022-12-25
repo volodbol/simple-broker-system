f0c5e9a1f84fc7238899f28a8676f490c3be9128
Volodymyr Bolshakov <volodbol@gmail.com>
2022-12-25 16:59:32 +0200

    feat: add meta annotation

---
+++ src/test/java/com/task/broker/IntegrationTest.java
@@ -0,0 +1,16 @@
+package com.task.broker;
+
+import org.junit.jupiter.api.Tag;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("integration")
+@ActiveProfiles("test")
+public @interface IntegrationTest {
+}
