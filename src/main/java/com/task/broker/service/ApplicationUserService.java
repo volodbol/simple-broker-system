de99cfaa04b5bfeae249721d08370dfb75da54d3
Volodymyr Bolshakov <volodbol@gmail.com>
2022-12-25 12:02:45 +0200

    feat: add method for top up balance

--- src/main/java/com/task/broker/service/ApplicationUserService.java
+++ src/main/java/com/task/broker/service/ApplicationUserService.java
@@ -1,10 +1,15 @@
 package com.task.broker.service;
 
+import com.task.broker.dto.BalanceTopUpDto;
 import com.task.broker.model.ApplicationUser;
 import com.task.broker.repository.ApplicationUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Service
@@ -15,6 +20,14 @@ public class ApplicationUserService {
 
     public Optional<ApplicationUser> findUser(String login) {
         return applicationUserRepository.findByLogin(login);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
+    public ApplicationUser topUpUserBalance(BalanceTopUpDto balanceTopUpDto, ApplicationUser applicationUser) {
+        BigDecimal currentBalance = applicationUser.getBalance();
+        BigDecimal topUppedBalance = currentBalance.add(balanceTopUpDto.getTopUpValue());
+        applicationUser.setBalance(topUppedBalance);
+        return applicationUserRepository.save(applicationUser);
     }
 
 }
